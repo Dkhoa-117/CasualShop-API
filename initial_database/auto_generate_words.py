c14f01dc64cd6349f1c6e0950842b67aef2949ec
Vũ Đăng Khoa <dangkhoa11227@gmail.com>
2023-03-13 11:19:04 +0700

    add data

--- initial_database/auto_generate_words.py
+++ initial_database/auto_generate_words.py
@@ -10,43 +10,34 @@ words = response.content.splitlines()
 def WordGen(numOfWord):
     result = ' '.join([words[random.randint(0,len(words))].decode('utf-8').capitalize() for _ in range(numOfWord)])
     return result
-def UserGenerate(num):
-    gender = ['male', 'female', 'other']
-    template = "('{0}', '{1}', '{2}', NOW(), '{3}@gmail.com', '/default_avatar', NULL, NULL)"
-    result = ', '.join([template.format(WordGen(2), gender[random.randint(0, 2)], WordGen(3).lower().replace(' ', ''), WordGen(2).lower().replace(' ', '')) for _ in range(num)])
-    return result
-def MovieGen(num):
-    catagory = ['action', 'adventure', 'comedy', 'drama', 'fantasy', 'horror', 'musical', 'mystery', 'romance', 'science_fiction', 'sports', 'thriller', 'western']
-    template = "('{0}', '/spiderman.png', '{1}', {2}, {3}, '{4}', NULL, NULL)"
-    result = ', '.join([template.format(WordGen(random.randint(1, 5)), WordGen(random.randint(20, 30)),  str(random.randint(500000000, 1000000000)), str(random.randrange(50000000, 170000000)), catagory[random.randint(0, len(catagory))]) for _ in range(num)])
-    return result
-def PersonGen(num):
-    gender = ['male', 'female', 'others']
-    department = ['acting', 'writing', 'directing']
-    template = "('{0}', NOW(), '{1}', '{2}', '{3}', '{4}', '/default_avatar.png', NOW())"
-    result = ', '.join([template.format(WordGen(2), department[random.randint(0, 2)], gender[random.randint(0, 2)], WordGen(2), ', '.join(WordGen(3).split())) for _ in range(num)])
-    return result
 
-# NOTE: print(UserGenerate(10))
-# NOTE: print(MovieGen(30))
+sub_category = ['gods','creatures', 'items', 'notebook', 'novel', 'key_chain', 'backpack_wallet', 'necklace']
+class Product:
+    name = WordGen(5)
+    img_path = '/image00{0}'.format(random.randint(0, 9))
+    description = WordGen(30)
+    price = random.randint(10, 3000)
+    category = sub_category[random.randint(0, 7)]
+    discount_id = random.randint(0, 20)
+    num_product = random.randint(50, 200)
+    num_buy = random.randint(1, 600)
+    like = random.randint(0, 200)
+    rating = random.randint(0, 5)
 
+listOfProduct = [];
+for _ in range(50):
+    listOfProduct.append(Product)
 def ProductGen(num):
-    template = ''
-    result = ', '.join([template.format() for _ in range(num)])
-    return result
-def CategoryGen(num):
-    category = ['first']
-    sub_category = []
-    template = ''
-    result = ', '.join([template.format() for _ in range(num)])
-    return result
-def DiscountGen(num):
-    template = ''
-    result = ', '.join([template.format() for _ in range(num)])
+    template = "('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}')"
+    result = ', '.join([template.format(listOfProduct[i].name, listOfProduct[i].description, listOfProduct[i].price, listOfProduct[i].category, listOfProduct[i].discount_id, listOfProduct[i].num_product, listOfProduct[i].num_buy, listOfProduct[i].like, listOfProduct[i].rating) for i in range(num)])
     return result
 
-logging.info(ProductGen(60))
+def DiscountGen(num):
+    template = "('{0}', '{1}')"
+    result = ', '.join([template.format(WordGen(2), random.randint(25, 70)) for _ in range(num)])
+    return result
+
+logging.info(ProductGen(50))
 logging.info('-'.join(['-' for _ in range(10)]))
-logging.info(CategoryGen(10))
-logging.info('-'.join(['-' for _ in range(10)]))
-logging.info(DiscountGen(10))
+logging.info(DiscountGen(20))
+
