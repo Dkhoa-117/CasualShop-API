2317f2ab7c765d32beed89885da6ef11e079ebd3
Vũ Đăng Khoa <dangkhoa11227@gmail.com>
2023-03-15 10:43:18 +0700

    fix cors

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ dotenv.config();
 
 // routes
 app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Origin",
+		"casualshop-api-production.up.railway.app"
+	);
 	res.header(
 		"Access-Control-Allow-Headers",
 		"Origin, X-Requested-With, Content-Type, Accept"
