1b3daf43a31c6c00ebd8a1750f23d6bb0e9dfb0a
Vũ Đăng Khoa <dangkhoa11227@gmail.com>
2023-03-17 22:36:40 +0700

    use cors package

--- index.js
+++ index.js
@@ -4,17 +4,19 @@ import router from "./routes/index.js";
 import errorHandler from "./middlewares/error-handler.js";
 import notFoundHandler from "./middlewares/not-found.js";
 import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
+app.use(cors());
 
 // routes
-app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-	res.header(
-		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-With, Content-Type, Accept"
-	);
-	next();
-});
+// app.use((req, res, next) => {
+// 	res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+// 	res.header(
+// 		"Access-Control-Allow-Headers",
+// 		"Origin, X-Requested-With, Content-Type, Accept"
+// 	);
+// 	next();
+// });
 app.use("/images", express.static("images"));
 app.use("/api/v1/product", router.product);
 app.use("/api/v1/category", router.category);
