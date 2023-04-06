d08481fbec809d2c758068ed82ef5580ef28b607
Vũ Đăng Khoa <dangkhoa11227@gmail.com>
2023-04-06 22:34:10 +0700

    reformat category

--- controllers/category.js
+++ controllers/category.js
@@ -7,7 +7,27 @@ export default {
 			if (err) throw err;
 			con.query("SELECT * FROM product_category", (err, data) => {
 				if (err) throw err;
-				res.status(200).json({ data });
+				const category = {
+					figurines: [],
+					books: [],
+					accessories: [],
+				};
+				data.map((item) => {
+					switch (item.name) {
+						case "figurines":
+							category.figurines.push(item.subCategory);
+							break;
+						case "books":
+							category.books.push(item.subCategory);
+							break;
+						case "accessories":
+							category.accessories.push(item.subCategory);
+							break;
+						default:
+							break;
+					}
+				});
+				res.status(200).json({ data: category });
 				con.release();
 			});
 		});
